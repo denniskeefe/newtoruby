d7ed0a04b58675cf51df738257f1ef8eb46b50c7
Dennis Keefe <dkeefe75@gmail.com>
2013-03-10 21:49:20 -0500

    compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Noobs::Application.configure do
   config.assets.precompile += %w( bootstrap.css )
+
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -16,7 +18,7 @@ Noobs::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
