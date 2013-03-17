ffa2cd226dbd5639abfa6325edb51e841a20a29f
Dennis Keefe <dkeefe75@gmail.com>
2013-03-16 22:22:54 -0500

    chartbeat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	before_filter :authenticate_user!, except: [:index, :show]
+	#before_filter :authenticate_user!, except: [:index, :show]
 
   protect_from_forgery
 end
