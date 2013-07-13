c397cb1f6610f6f86475b65390d965d683d3a321
Dennis Keefe <dkeefe75@gmail.com>
2013-07-13 13:37:54 -0500

    undo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,6 @@
 module ApplicationHelper
 
-	<% def google_analytics_js %>
- 
-'<script type="text/javascript">
- 
-var _gaq = _gaq || [];
- 
-_gaq.push(['_setAccount', 'UA-42413187-1']);
- 
-_gaq.push(['_trackPageview']);
- 
-(function() {
- 
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- 
-ga.src = ('https:' == document.location.protocol ? '<a href="https://ssl/">https://ssl</a>' : '<a href="http://www/">http://www</a>') + '.<a href="http://google-analytics.com/ga.js">google-analytics.com/ga.js</a>';
- 
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
- 
-})();
- 
-</script>'
- 
-<% end %>
+	#added analytics july13
+
+	
 end
